b3c33f7a2d882eade50ebce7f36407bce274048e
Abhay Manoj <melchior88729@gmail.com>
2026-02-17 04:38:18 -0500

    think i frogot to do the other file

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -6,17 +6,18 @@ private:
   static constexpr int STACK_SIZE = 16;
   static constexpr int REGISTER_COUNT = 16;
   static constexpr int MEMORY_SIZE = 4096;
+  static constexpr int STARTING_ADDRESS = 0x200;
   static constexpr int FREQUENCY = 432; // audio frequency
 
-  std::array<uint16_t, STACK_SIZE> stack{};  // stores return addresses
-  std::array<uint8_t, REGISTER_COUNT> V{};   // registers 0 - F
-  std::array<uint8_t, MEMORY_SIZE> memory{}; //
+  std::array<uint16_t, STACK_SIZE> stack{}; // stores return addresses
+  std::array<uint8_t, REGISTER_COUNT> V{};  // registers 0 - F
+  std::array<uint8_t, MEMORY_SIZE> memory{};
 
-  uint16_t I = 0;  // stores memory addresses, use 12 lowest bits
-  uint16_t PC = 0; // currently executing address
-  uint8_t SP = 0;  // topmost level of the stack
-  uint8_t DT = 0;  // delay timer register
-  uint8_t ST = 0;  // sound timer register, sound will play if 0
+  uint16_t I = 0;                 // stores memory addresses, use 12 lowest bits
+  uint16_t PC = STARTING_ADDRESS; // currently executing address
+  uint8_t SP = 0;                 // topmost level of the stack
+  uint8_t DT = 0;                 // delay timer register
+  uint8_t ST = 0;                 // sound timer register, play if > 0
 public:
   Chip8();
   Chip8(std::array<uint8_t, MEMORY_SIZE> memory);
