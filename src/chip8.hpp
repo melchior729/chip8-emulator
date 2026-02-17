318c72f22bec33466ddc24e5a19c036ff9bb776c
Abhay Manoj <melchior88729@gmail.com>
2026-02-17 04:33:20 -0500

    added chip8 constructors

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -2,12 +2,11 @@
 #include <cstdint>
 
 class Chip8 {
-public:
 private:
   static constexpr int STACK_SIZE = 16;
   static constexpr int REGISTER_COUNT = 16;
   static constexpr int MEMORY_SIZE = 4096;
-  static constexpr int FREQUENCY = 432;
+  static constexpr int FREQUENCY = 432; // audio frequency
 
   std::array<uint16_t, STACK_SIZE> stack{};  // stores return addresses
   std::array<uint8_t, REGISTER_COUNT> V{};   // registers 0 - F
@@ -18,4 +17,9 @@ private:
   uint8_t SP = 0;  // topmost level of the stack
   uint8_t DT = 0;  // delay timer register
   uint8_t ST = 0;  // sound timer register, sound will play if 0
+public:
+  Chip8();
+  Chip8(std::array<uint8_t, MEMORY_SIZE> memory);
+
+  void load_into_memory(std::array<uint8_t, MEMORY_SIZE> memory);
 };
