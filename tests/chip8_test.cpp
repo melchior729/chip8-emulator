18261549740369cfb739184976bb970e5e5bd925
Abhay Manoj <melchior88729@gmail.com>
2026-02-18 17:08:06 -0500

    Renamed testing file to lowercase

---
+++ tests/chip8_test.cpp
@@ -0,0 +1,15 @@
+#include "../src/chip8.hpp"
+#include <gtest/gtest.h>
+
+class Chip8Test : public ::testing::Test {
+protected:
+  void SetUp() override {
+    //
+    //
+  }
+
+  void TearDown() override {
+    //
+    //
+  }
+};
