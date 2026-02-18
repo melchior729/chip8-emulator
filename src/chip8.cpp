f01fa874190300671b29d398e9be5b87f0b8852a
Abhay Manoj <melchior88729@gmail.com>
2026-02-18 00:13:39 -0500

    Added Chip8:: to MEMORY_SIZE; Added incomplete sys function

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,10 +1,12 @@
 #include "chip8.hpp"
 
 Chip8::Chip8() {}
-Chip8::Chip8(std::array<uint8_t, MEMORY_SIZE> memory) {
+Chip8::Chip8(std::array<uint8_t, Chip8::MEMORY_SIZE> memory) {
   load_into_memory(memory);
 }
 
-void Chip8::load_into_memory(std::array<uint8_t, MEMORY_SIZE> memory) {
+void Chip8::load_into_memory(std::array<uint8_t, Chip8::MEMORY_SIZE> memory) {
   this->memory = memory;
 }
+
+void Chip8::sys(uint16_t address) { this->PC = address & 0xFFF; }
