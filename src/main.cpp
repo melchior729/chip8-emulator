7ac2178f2201817ce19ca8434a8116daff79f646
Abhay Manoj <melchior88729@gmail.com>
2026-02-28 23:45:15 -0500

    Finished the basic screen output

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 /// @date Feb 27 2026
 #include "chip8.hpp"
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
+#include <fstream>
+#include <memory>
 
 static constexpr size_t SCALING_FACTOR = 16;
 static constexpr size_t WINDOW_HEIGHT = Chip8::HEIGHT * SCALING_FACTOR;
@@ -16,28 +19,57 @@ static constexpr size_t WINDOW_WIDTH = Chip8::WIDTH * SCALING_FACTOR;
 struct AppState {
   SDL_Window *window = nullptr;
   SDL_Renderer *renderer = nullptr;
-  Chip8 cpu;
+  std::unique_ptr<Chip8> cpu;
 };
+
+/// @brief loads the rom from the system into the appstate
+/// @param appstate contains the current appstate
+/// @param file_path the path of the rom to load
+SDL_AppResult load_rom(void *appstate, const std::string &file_path) {
+  AppState *state = static_cast<AppState *>(appstate);
+  std::ifstream file(file_path, std::ios::binary);
+  if (!file) {
+    return SDL_APP_FAILURE;
+  }
+
+  auto rom = std::make_unique<std::array<uint8_t, Chip8::MEMORY_SIZE>>();
+  file.read(reinterpret_cast<char *>(rom->data()), rom->size());
+  state->cpu->load_into_memory(*rom);
+  return SDL_APP_CONTINUE;
+}
 
 /// @brief initializes SDL, the window and the renderer
 /// @param appstate contains the current appstate
 /// @param argc the number of arguments
 /// @param argv the arguments
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+  constexpr int NUM_ARGS = 2;
+
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     return SDL_APP_FAILURE;
   }
 
+  if (argc != NUM_ARGS) {
+    SDL_Log("Usage: %s <rom path>", argv[0]);
+    return SDL_APP_FAILURE;
+  }
+
   AppState *state = new AppState();
+  state->cpu = std::make_unique<Chip8>();
+  *appstate = state;
 
   if (!SDL_CreateWindowAndRenderer("Abhay's Chip8 Emulator", WINDOW_WIDTH,
                                    WINDOW_HEIGHT, 0, &state->window,
                                    &state->renderer)) {
     delete state;
+    *appstate = nullptr;
     return SDL_APP_FAILURE;
   }
 
-  *appstate = state;
+  if (load_rom(*appstate, argv[1]) != SDL_APP_CONTINUE) {
+    return SDL_APP_FAILURE;
+  }
+
   return SDL_APP_CONTINUE;
 }
 
@@ -55,9 +87,8 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 /// @brief draws the content of the display buffer to the SDL window
 /// @param appstate contains the current appstate
 static void draw_to_screen(void *appstate) {
-
   AppState *state = static_cast<AppState *>(appstate);
-  const auto &display_buffer = state->cpu.get_display_buffer();
+  const auto &display_buffer = state->cpu->get_display_buffer();
   std::array<SDL_FRect, Chip8::WIDTH * Chip8::HEIGHT> pixels;
   size_t pixel_count = 0;
 
@@ -72,6 +103,7 @@ static void draw_to_screen(void *appstate) {
     }
   }
 
+  SDL_SetRenderDrawColor(state->renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
   SDL_RenderFillRects(state->renderer, pixels.data(), pixel_count);
 }
 
@@ -82,6 +114,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
   SDL_RenderClear(state->renderer);
   draw_to_screen(appstate);
+  state->cpu->cycle();
   SDL_RenderPresent(state->renderer);
   return SDL_APP_CONTINUE;
 }
